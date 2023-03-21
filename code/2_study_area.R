c31a4be4de92366f92fbc4e2d1f95693e98dcaaf
bpfree <brian.patrick.free@gmail.com>
2023-03-21 18:47:11 -0400

    Update 10-acre grid, subset hexagonal grid to only Oregon call areas

--- code/2_study_area.R
+++ code/2_study_area.R
@@ -44,13 +44,11 @@ pacman::p_load(dplyr,
 # Set directories
 ## Define data directory (as this is an R Project, pathnames are simplified)
 ### Input directories
-land_dir <- "data/a_raw_data/USGSEsriWCMC_GlobalIslands_v3/v108/globalislandsfix.gdb"
 wind_area_dir <- "data/a_raw_data/BOEM-Renewable-Energy-Geodatabase/BOEMWindLayers_4Download.gdb"
 
 ### Output directories
 #### Analysis directories
 analysis_gpkg <- "data/c_analysis_data/gom_cable_study.gpkg"
-raster_dir <- "data/d_raster_data"
 
 #### Intermediate directories
 study_area_gpkg <- "data/b_intermediate_data/oregon_study_area.gpkg"
@@ -59,27 +57,41 @@ wind_farm_gpkg <- "data/b_intermediate_data/oregon_wind_area.gpkg"
 #####################################
 #####################################
 
+# Load call areas
 wind_areas <- sf::st_read(wind_area_dir, "Wind_Planning_Area_Outlines_2_2023") %>%
   # filter for only Oregon call areas
   dplyr::filter(grepl("Oregon", CATEGORY1)) %>%
   # reproject data into a coordinate system that will convert units from degrees to meters
   sf::st_transform("EPSG:26910")
 
-# verify that units are in meters and no longer degrees
+## verify that units are in meters and no longer degrees
 st_crs(wind_areas, parameters = TRUE)$units_gdal
 
 #####################################
 #####################################
 
-wind_area_hex <- sf::st_make_grid(x = wind_areas,
-                                  # cellsize to generate 10-acre hexagons (area = 40468.6 m^2, side = 124.80533 m)
-                                  ## see documentation on what cellsize means when relating to hexagons: https://github.com/r-spatial/sf/issues/1505
-                                  ## cellsize is the distance between two vertices (short diagonal --> d = square root of 3 * length of side)
-                                  ### So in this case, square-root of 3 * 123.80533 = 1.73205080757 * 124.80533 = 216.169172615
-                                  cellsize = 216.169172615,
-                                  # make hexagon (TRUE will generate squares)
-                                  square = FALSE,
-                                  # make hexagons orientation with a flat topped (FALSE = pointy top)
-                                  flat_topped = TRUE)
+# Create 10-acre grid around call areas
+wind_area_grid <- sf::st_make_grid(x = wind_areas,
+                                   ## see documentation on what cellsize means when relating to hexagons: https://github.com/r-spatial/sf/issues/1505
+                                   ## cellsize is the distance between two vertices (short diagonal --> d = square root of 3 * length of side)
+                                   ### So in this case, square-root of 3 * 123.80533 = 1.73205080757 * 124.80533 = 216.169172615
+                                   cellsize = 216.169172615,
+                                   # make hexagon (TRUE will generate squares)
+                                   square = FALSE,
+                                   # make hexagons orientation with a flat topped (FALSE = pointy top)
+                                   flat_topped = TRUE) %>%
+  # convert back as sf
+  sf::st_as_sf()
 
+#####################################
+#####################################
+
+# Subset by location: hexagonal grids that intersect with wind areas
+wind_area_hex <- wind_area_grid[wind_areas, ]
+
+#####################################
+#####################################
+
+# Export data
+sf::st_write(wind_area_grid, dsn = wind_farm_gpkg, layer = "oregon_call_area_grid", append = F)
 sf::st_write(wind_area_hex, dsn = wind_farm_gpkg, layer = "oregon_call_area_hex", append = F)
