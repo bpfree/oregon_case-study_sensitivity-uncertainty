4f890511d88c7d51a6dcf5e1d3ec277c121cc6e3
bpfree <brian.patrick.free@gmail.com>
2023-03-29 10:46:45 -0400

    Clean code

--- code/0_directory_creation.R
+++ code/0_directory_creation.R
@@ -7,13 +7,13 @@ data_dir <- dir.create("data")
 
 # Designate subdirectories
 data_subdirectories <- c("a_raw_data",
-                      "b_intermediate_data",
-                      "c_submodel_data",
-                      "d_suitability_data",
-                      "e_rank_data",
-                      "f_sensitivity_data",
-                      "g_uncertainty_data",
-                      "zz_miscellaneous")
+                         "b_intermediate_data",
+                         "c_submodel_data",
+                         "d_suitability_data",
+                         "e_rank_data",
+                         "f_sensitivity_data",
+                         "g_uncertainty_data",
+                         "zz_miscellaneous")
 
 # Create sub-directories within data directory
 for (i in 1:length(data_subdirectories)){
