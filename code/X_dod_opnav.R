94c7dfe23d49f7de742b154ee4836ec98c5f0a84
bpfree <brian.patrick.free@gmail.com>
2023-04-07 14:03:22 -0400

    DoD OPNAV data

---
+++ code/X_dod_opnav.R
@@ -0,0 +1,88 @@
+#######################################
+### X. DoD Offshore Wind Assessment ###
+#######################################
+
+# Clear environment
+rm(list = ls())
+
+# Load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,
+               fasterize,
+               fs,
+               ggplot2,
+               janitor,
+               pdftools,
+               plyr,
+               raster,
+               rgdal,
+               rgeos,
+               rmapshaper,
+               rnaturalearth, # use devtools::install_github("ropenscilabs/rnaturalearth") if packages does not install properly
+               sf,
+               sp,
+               terra, # is replacing the raster package
+               tidyr)
+
+#####################################
+#####################################
+
+# Set directories
+## Define data directory (as this is an R Project, pathnames are simplified)
+### Input directories
+dod_opnav_dir <- "data/a_raw_data/opnav_combined_assessment_May2022"
+
+study_area_gpkg <- "data/b_intermediate_data/oregon_study_area.gpkg"
+wind_area_gpkg <- "data/b_intermediate_data/oregon_wind_area.gpkg"
+
+### Output directories
+#### Constraint directories
+constraint_geopackage <- "data/c_submodel_data/constraints.gpkg"
+
+#### Intermediate directories
+intermediate_dir <- "data/b_intermediate_data"
+dod_gpkg <- "data/b_intermediate_data/pacpars.gpkg"
+
+#####################################
+#####################################
+
+# Load data
+## Oregon Call Areas
+oregon_call_areas <- sf::st_read(dsn = wind_area_gpkg,
+                                 layer = paste(sf::st_layers(dsn = wind_area_gpkg,
+                                                             do_count = TRUE)))
+
+## Oregon hex areas
+oregon_hex <- sf::st_read(dsn = study_area_gpkg,
+                          layer = paste(sf::st_layers(dsn = study_area_gpkg,
+                                                      do_count = TRUE)[[1]][2]))
+
+#####################################
+
+## DoD combined Oregon offshore wind assessment (OPNAV May 2022)
+### ***Note: currently these data are not available for download 
+### A data portal contains the data for viewing: https://offshorewind.westcoastoceans.org/
+### To view the data: Human > Military > Combined Oregon Offshore Wind Assessment, OPNAV, May 2022
+### Metadata: https://www.coastalatlas.net/waf/boem/OPNAV_CombinedAssesment_May2022.xml
+### Dataset Source Provider: https://gis.lcd.state.or.us/server/rest/services/Projects/OCMP_OceanPlanning_Human/MapServer/21
+### Alternative link on dataset: https://portal.westcoastoceans.org/geoportal/rest/metadata/item/45b6aa29abe7427a91d8f430eac0ab75/html
+### InPort: https://www.fisheries.noaa.gov/inport/item/48875
+
+### Explaination of data
+#### Green = no mitigation necessary -- open areas
+#### Red = mitigation not feasible -- closed areas
+
+dod_opnav <- sf::st_read(dsn = dod_opnav_dir, layer = "OPNAV_CombinedAssesment_May2022") %>%
+  # reproject data into a coordinate system (NAD 1983 UTM Zone 10N) that will convert units from degrees to meters
+  sf::st_transform("EPSG:26910")  %>%
+  # limit areas to only ones that will be constraints (no mitigation feasible)
+  dplyr::filter(Color == "Red") %>%
+  # obtain only submarine cables in the study area
+  rmapshaper::ms_clip(target = .,
+                      clip = oregon_call_areas) %>%
+  # create field called "layer" and fill with "submarine cables" for summary
+  dplyr::mutate(layer = "DoD exclusion")
+
+#####################################
+
+oregon_hex_dod_opnav <- oregon_hex[dod_opnav, ]
