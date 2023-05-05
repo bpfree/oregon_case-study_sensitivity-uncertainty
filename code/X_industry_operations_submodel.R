f6e98f5ba5802c4f3a0f4b6cf6a8488a16c954dc
bpfree <brian.patrick.free@gmail.com>
2023-05-05 14:22:52 -0400

    Industry and operations submodel

---
+++ code/X_industry_operations_submodel.R
@@ -0,0 +1,34 @@
+###########################################
+### X. Industry and Operations Submodel ###
+###########################################
+
+# Clear environment
+rm(list = ls())
+
+# Load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,
+               fasterize,
+               fs,
+               ggplot2,
+               janitor,
+               pdftools,
+               plyr,
+               raster,
+               rgdal,
+               rgeos,
+               rmapshaper,
+               rnaturalearth, # use devtools::install_github("ropenscilabs/rnaturalearth") if packages does not install properly
+               sf,
+               sp,
+               terra, # is replacing the raster package
+               tidyr)
+
+
+#####################################
+#####################################
+
+# Set directories
+## Input directories
+### Submodel
+industry_operations_gpkg <- "data/c_submodel_data/constraints.gpkg"
