03084f3b4e0d34943927966db2a2b071d4cbf1e0
bpfree <brian.patrick.free@gmail.com>
2023-05-05 14:22:39 -0400

    Constraints submodel

---
+++ code/X_constraints_submodel.R
@@ -0,0 +1,41 @@
+###############################
+### X. Constraints Submodel ###
+###############################
+
+# Clear environment
+rm(list = ls())
+
+# Load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,
+               fasterize,
+               fs,
+               ggplot2,
+               janitor,
+               pdftools,
+               plyr,
+               raster,
+               rgdal,
+               rgeos,
+               rmapshaper,
+               rnaturalearth, # use devtools::install_github("ropenscilabs/rnaturalearth") if packages does not install properly
+               sf,
+               sp,
+               terra, # is replacing the raster package
+               tidyr)
+
+
+#####################################
+#####################################
+
+# Set directories
+## Input directories
+industry_operations_submodel <- "data/c_submodel_data/industry_operations_submodel.gpkg"
+
+#####################################
+#####################################
+
+# Load data
+## Submarine cable
+
+## NMFS Scientific Survey
