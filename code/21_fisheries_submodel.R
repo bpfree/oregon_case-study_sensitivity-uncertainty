930af78fec4db19256996f5c13bbdc31e7319c4c
bpfree <brian.patrick.free@gmail.com>
2023-06-01 17:33:49 -0400

    Create fisheries submodel data

--- code/21_fisheries_submodel.R
+++ code/21_fisheries_submodel.R
@@ -34,13 +34,33 @@ study_area_gpkg <- "data/b_intermediate_data/oregon_study_area.gpkg"
 fisheries_submodel <- "data/c_submodel_data/fisheries_submodel.gpkg"
 
 ## Output directories
-oregon_fisheries_suitability <- "data/c_submodel_data/fisheries_suitability.gpkg"
+### Oregon suitability geopackage
 oregon_suitability_gpkg <- "data/d_suitability_data/suitability_model.gpkg"
+
+### Fisheries directory
+suitability_dir <- "data/d_suitability_data"
+dir.create(paste0(suitability_dir, "/",
+                  "fisheries_suitability"))
+
+oregon_fisheries_dir <- "data/d_suitability_data/fisheries_suitability"
+oregon_fisheries_suitability <- "data/d_suitability_data/fisheries_suitability/fisheries_suitability.gpkg"
 
 #####################################
 
 sf::st_layers(dsn = fisheries_submodel,
               do_count = T)
+
+#####################################
+#####################################
+
+clean_function <- function(data){
+  
+  data <- data %>%
+    as.data.frame() %>%
+    dplyr::select(-geom)
+  
+  return(data)
+}
 
 #####################################
 #####################################
@@ -52,4 +72,55 @@ oregon_hex <- sf::st_read(dsn = study_area_gpkg,
                                                       do_count = TRUE)[[1]][2]))
 
 ## Fisheries
+oregon_fisheries_nmfs_odfw <- sf::st_read(dsn = fisheries_submodel,
+                                          layer = "oregon_hex_fisheries")
 
+#####################################
+#####################################
+
+# Prepare the fisheries data
+oregon_fisheries_values <- oregon_fisheries_nmfs_odfw %>%
+  clean_function()
+
+#####################################
+#####################################
+
+# Calculate geometric mean for industry and operations submodel
+oregon_fisheries <- oregon_hex %>%
+  # join the fisheries values by index field to full Oregon call area hex grid
+  dplyr::left_join(x = .,
+                   y = oregon_fisheries_values,
+                   by = "index") %>%
+  # calculate across rows
+  dplyr::rowwise() %>%
+  # calculate the geometric mean
+  ## geometric mean = nth root of the product of the variable values
+  dplyr::mutate(fish_geom_mean = exp(mean(log(c_across(c("fisheries_index"))),
+                                          # remove any values that are NA when calculating the mean
+                                          na.rm = T))) %>%
+  # select the fields of interest
+  dplyr::select(index,
+                fisheries_index,
+                fish_geom_mean) %>%
+  dplyr::rename(fisheries_value = fisheries_index)
+
+### Check to see if there are any duplicates of the indices
+### There are none
+fish_duplicates <- oregon_fisheries %>%
+  # create frequency field based on index
+  dplyr::add_count(index) %>%
+  # see which ones are duplicates
+  dplyr::filter(n>1) %>%
+  # show distinct options
+  dplyr::distinct()
+
+#####################################
+#####################################
+
+# Export data
+## Suitability
+sf::st_write(obj = oregon_fisheries, dsn = oregon_suitability_gpkg, layer = "oregon_fisheries_suitability", append = F)
+
+## Submodel
+### Fisheries
+saveRDS(obj = oregon_fisheries_values, file = paste(oregon_fisheries_dir, "oregon_fisheries_values.rds", sep = "/"))
