d1025fc904e7f23708fdd71ffd75fe44fad5decd
bpfree <brian.patrick.free@gmail.com>
2023-07-20 08:42:15 -0600

    Match packages across scripts, set default parameters, update pathnames and data names, streamline export data names, calculate time code takes to complete

--- code/20_natural_resources_submodel.R
+++ code/20_natural_resources_submodel.R
@@ -5,25 +5,35 @@
 # Clear environment
 rm(list = ls())
 
+# Calculate start time of code (determine how long it takes to complete all code)
+start <- Sys.time()
+
+#####################################
+#####################################
+
 # Load packages
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(dplyr,
+pacman::p_load(docxtractr,
+               dplyr,
+               elsa,
                fasterize,
                fs,
                ggplot2,
                janitor,
+               ncf,
                pdftools,
                plyr,
                raster,
                rgdal,
+               rgeoda,
                rgeos,
                rmapshaper,
                rnaturalearth, # use devtools::install_github("ropenscilabs/rnaturalearth") if packages does not install properly
                sf,
                sp,
+               stringr,
                terra, # is replacing the raster package
                tidyr)
-
 
 #####################################
 #####################################
@@ -43,7 +53,16 @@ dir.create(paste0(suitability_dir, "/",
                   "natural_resources_suitability"))
 
 oregon_natural_resources_dir <- "data/d_suitability_data/natural_resources_suitability"
-oregon_natural_resources_suitability <- "data/d_suitability_data/natural_resources_suitability/natural_resources_suitability.gpkg"
+dir.create(paste0(oregon_natural_resources_dir, "/",
+                  "protected_species"))
+dir.create(paste0(oregon_natural_resources_dir, "/",
+                  "habitat"))
+dir.create(paste0(oregon_natural_resources_dir, "/",
+                  "marine_bird"))
+
+oregon_nr_species_dir <- "data/d_suitability_data/natural_resources_suitability/protected_species"
+oregon_nr_habitat_dir <- "data/d_suitability_data/natural_resources_suitability/habitat"
+oregon_nr_marine_bird_dir <- "data/d_suitability_data/natural_resources_suitability/marine_bird"
 
 #####################################
 
@@ -61,6 +80,18 @@ clean_function <- function(data){
   
   return(data)
 }
+
+#####################################
+#####################################
+
+## designate region name
+region <- "oregon"
+
+## submodel
+submodel <- "natural_resources"
+
+## designate date
+date <- format(Sys.time(), "%Y%m%d")
 
 #####################################
 #####################################
@@ -85,11 +116,12 @@ non_protected_species_areas <- sf::st_read(dsn = natural_resources_submodel, lay
 efhca <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_efhca_500m")
 rocky_reef_mapped <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_rocky_reef_mapped_500m")
 rocky_reef_probable <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_rocky_reef_probable_500m")
-deep_sea_coral_sponge <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_high_habitat_coral_sponge")
+deep_sea_coral_sponge <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_high_habitat_coral_sponge_500m")
 continental_shelf <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_continental_shelf_10km")
 methane_bubble <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_methane_bubble_streams_1km")
 
-## Seabird
+## Marine bird
+marine_bird <- sf::st_read(dsn = natural_resources_submodel, layer = "oregon_hex_marine_bird")
 
 #####################################
 #####################################
@@ -211,7 +243,7 @@ continental_shelf_value <- continental_shelf %>%
   dplyr::mutate(continental_shelf_value = 0.6) %>%
   clean_function()
 
-### Methane bubble streans
+### Methane bubble streams
 methane_bubble_value <- methane_bubble %>%
   dplyr::mutate(methane_bubble_value = 0.8) %>%
   clean_function()
@@ -273,6 +305,13 @@ habitat_values <- oregon_hex %>%
   as.data.frame()
 
 #####################################
+
+### Marine bird values
+marine_bird_value <- marine_bird %>%
+  clean_function() %>%
+  dplyr::rename(marine_bird_value = marine_bird_index)
+
+#####################################
 #####################################
 
 # Calculate geometric mean for natural resources submodel
@@ -283,15 +322,16 @@ oregon_natural_resources <- oregon_hex %>%
   dplyr::left_join(x = .,
                    y = habitat_values,
                    by = "index") %>%
-#  dplyr::left_join(x = .,
-#                   y = seabird_values,
-#                   by = "index") %>%
+ dplyr::left_join(x = .,
+                  y = marine_bird_value,
+                  by = "index") %>%
   # calculate across rows
   dplyr::rowwise() %>%
   # calculate the geometric mean
   ## geometric mean = nth root of the product of the variable values
   dplyr::mutate(nr_geom_mean = exp(mean(log(c_across(c("species_product",
-                                                       "habitat_value"))),
+                                                       "habitat_value",
+                                                       "marine_bird_value"))),
                                         # remove any values that are NA when calculating the mean
                                         na.rm = T))) %>%
   # select the fields of interest
@@ -309,6 +349,7 @@ oregon_natural_resources <- oregon_hex %>%
                 continental_shelf_value,
                 methane_bubble_value,
                 habitat_value,
+                marine_bird_value,
                 nr_geom_mean) %>%
   # rename the geometry field
   dplyr::rename(geom = geom.x)
@@ -328,15 +369,34 @@ nr_duplicates <- oregon_natural_resources %>%
 
 # Export data
 ## Suitability
-sf::st_write(obj = oregon_natural_resources, dsn = oregon_suitability_gpkg, layer = "oregon_natural_resources_suitability", append = F)
+sf::st_write(obj = oregon_natural_resources, dsn = oregon_suitability_gpkg, layer = paste0(region, "_", submodel, "_suitability"), append = F)
 
 ## Submodel
 ### Protected species
-sf::st_write(obj = protected_species, dsn = oregon_natural_resources_suitability, layer = "oregon_protected_species_suitability", append = F)
+base::saveRDS(object = protected_species, file = paste(oregon_nr_species_dir, "oregon_protected_species_suitability.RDS", ))
+base::saveRDS(object = leatherback_value, file = paste(oregon_nr_species_dir, "oregon_leatherback_value.RDS", sep = "/"))
+base::saveRDS(object = killer_whale_value, file = paste(oregon_nr_species_dir, "oregon_killer_whale_value.RDS", sep = "/"))
+base::saveRDS(object = humpback_mx_value, file = paste(oregon_nr_species_dir, "oregon_humpback_mexico_value.RDS", sep = "/"))
+base::saveRDS(object = humpback_ca_value, file = paste(oregon_nr_species_dir, "oregon_humpback_central_america_value.RDS", sep = "/"))
+base::saveRDS(object = bluewhale_value, file = paste(oregon_nr_species_dir, "oregon_blue_whale_value.RDS", sep = "/"))
 
 ### Habitat layers
+base::saveRDS(object = habitat_values, file = paste(oregon_nr_habitat_dir, layer = "oregon_habitat_suitability.RDS", sep = "/"))
+base::saveRDS(object = efhca_value, file = paste(oregon_nr_habitat_dir, layer = "oregon_efhca_value.RDS", sep = "/"))
+base::saveRDS(object = rreef_map_value, file = paste(oregon_nr_habitat_dir, layer = "oregon_rocky_reef_mapped_500m_value.RDS", sep = "/"))
+base::saveRDS(object = rreef_prob_value, file = paste(oregon_nr_habitat_dir, layer = "oregon_rocky_reef_probable_500m_value.RDS", sep = "/"))
+base::saveRDS(object = coral_sponge_value, file = paste(oregon_nr_habitat_dir, layer = "oregon_deep_sea_coral_sponge_value.RDS", sep = "/"))
+base::saveRDS(object = continental_shelf_value, file = paste(oregon_nr_habitat_dir, layer = "oregon_continental_shelf_value.RDS", sep = "/"))
+base::saveRDS(object = methane_bubble_value, file = paste(oregon_nr_habitat_dir, layer = "oregon_methane_bubble_streams_value.RDS", sep = "/"))
 
 ### Seabird
+base::saveRDS(object = marine_bird_value, file = paste(oregon_nr_marine_bird_dir, layer = "oregon_marine_bird_value.RDS", sep = "/"))
 
 ### Suitability
 sf::st_write(obj = oregon_natural_resources, dsn = oregon_natural_resources_suitability, layer = "oregon_natural_resources_suitability", append = F)
+
+#####################################
+#####################################
+
+# calculate end time and print time difference
+print(Sys.time() - start) # print how long it takes to calculate
