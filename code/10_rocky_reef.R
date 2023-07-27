daf30210a20389da9484db70df87600ef457fad3
bpfree <brian.patrick.free@gmail.com>
2023-07-27 10:35:27 -0600

    Replace hex grid with the grid used in the original analysis

--- code/10_rocky_reef.R
+++ code/10_rocky_reef.R
@@ -74,15 +74,15 @@ date <- format(Sys.time(), "%Y%m%d")
 #####################################
 
 # Load data
-## Oregon hex grid
-oregon_hex <- sf::st_read(dsn = study_area_gpkg,
-                          layer = paste(sf::st_layers(dsn = study_area_gpkg,
-                                                      do_count = TRUE)[[1]][2]))
-
 ## Oregon call areas
 oregon_call_areas <- sf::st_read(dsn = wind_area_gpkg,
                                  layer = paste(sf::st_layers(dsn = wind_area_gpkg,
                                                              do_count = TRUE)))
+
+## Oregon hex areas (original data)
+oregon_hex <- sf::st_read(dsn = study_area_gpkg,
+                          layer = paste(sf::st_layers(dsn = study_area_gpkg,
+                                                      do_count = TRUE)[[1]][4]))
 
 #####################################
 
