cb41f1028ce892d58fd55560e740b85874532ff8
bpfree <brian.patrick.free@gmail.com>
2023-08-18 15:28:58 -0400

    Sensitivity cable table updates for calculating nominal and percent changes; create figures

--- code/28_sensitivity_change_table.R
+++ code/28_sensitivity_change_table.R
@@ -69,6 +69,21 @@ date <- format(Sys.time(), "%Y%m%d")
 #####################################
 #####################################
 
+plot_theme <- theme(panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    axis.title.x = element_text(size = 10),
+                    axis.text.x = element_text(size = 8),
+                    axis.title.y = element_text(size = 10),
+                    axis.text.y = element_text(size = 8),
+                    plot.subtitle = element_text(size = 8),
+                    plot.caption = element_text(size = 6,
+                                                lineheight = 0.5),
+                    legend.title = element_text(size = 8),
+                    legend.text = element_text(size = 5))
+
+#####################################
+#####################################
+
 # load data
 sensitivity_jackknife <- sf::st_read(dsn = sensitivity_gpkg, layer = paste(region, "sensitivity", sep = "_"))
 
@@ -86,10 +101,13 @@ for (i in 1:24){
   # if wanting to test a particular dataset
   #i <- 8
   
+  # identify indexed field for the original model's score
   original_model_score <- 33
   
+  # identify all jackknifed final scores
   p <- original_model_score + i*5
   
+  # grab the name of the datasent
   name_change <- names(sensitivity_jackknife)[p] %>%
     # extract name of leftout dataset
     ## ?<= will look back and exclude the match (mean_)
@@ -97,10 +115,15 @@ for (i in 1:24){
     ## ?= will look ahead and exclude the match (_value)
     stringr::str_extract(string = ., pattern = "(?<=mean_).*?(?=_value)")
   
+  # calculate the nominal and percent changes between dataset removal
   change_iteration <- sensitivity_jackknife %>%
+    # create nominal change ()
     dplyr::mutate(!!paste("model_geom_mean", name_change, "nominal_change", sep = "_") := .[[p]] - .[[original_model_score]],
+                  # move field after the dataset model geometric mean field
                   .after = paste0("model_geom_mean_", name_change, "_value")) %>%
-    dplyr::mutate(!!paste("model_geom_mean", name_change, "percent_change", sep = "_") := round(((.[[p]] - .[[original_model_score]]) / .[[p]]) * 100, 2) ,
+    # create percent change field ((new value - old value) / old value * 100)
+    dplyr::mutate(!!paste("model_geom_mean", name_change, "percent_change", sep = "_") := round(((.[[p]] - .[[original_model_score]]) / .[[original_model_score]]) * 100, 2),
+                  # move field after the dataset model nominal change field
                   .after = paste("model_geom_mean", name_change, "nominal_change", sep = "_")) %>%
     
     
@@ -134,9 +157,14 @@ for (i in 0:23){
   # if wanting to test a particular dataset
   #i <- 4
   
+  # identify all jackknifed changed score fields (nominal and percent changes)
+  ## nominal change
   nominal_data <- 3 + i*2
+  
+  ## percent change
   percent_data <- 4 + i*2
   
+  # grab the name of the datasent
   name_change <- names(sensitivity_nominal_percent_change)[nominal_data] %>%
     # extract name of leftout dataset
     ## ?<= will look back and exclude the match (mean_)
@@ -144,26 +172,33 @@ for (i in 0:23){
     ## ?= will look ahead and exclude the match (_value)
     stringr::str_extract(string = ., pattern = "(?<=mean_).*?(?=_nominal_change)")
   
+  # add the dataset layer name to the dataset list
+  ## this will allow for each row to have minimum and maximums
+  ## for nominal and percent changes by dataset
   dataset_list <- c(name_change)
   
+  # calculate minimum and maximum nonimal changes
+  ## minimum
   n_min <- min(sensitivity_nominal_percent_change[[nominal_data]])
+  ## maximim
   n_max <- max(sensitivity_nominal_percent_change[[nominal_data]])
   
+  # calculate minimum and maximum percent changes
+  ## minimum
   p_min <- min(sensitivity_nominal_percent_change[[percent_data]])
+  ## maximum
   p_max <- max(sensitivity_nominal_percent_change[[percent_data]])
   
+  # get resulting table with nominal and percent change minimums and maximums
   result_table <- data.frame(
     dataset_list,
     n_min,
     n_max,
     p_min,
-    p_max
-  )
+    p_max)
   
+  # bind the resulting table to the original table
   table <- rbind(table, result_table)
-  
-  hist_nominal <- hist(sensitivity_nominal_percent_change[[nominal_data]])
-  hist_percent <- hist(sensitivity_nominal_percent_change[[percent_data]])
   
   # print how long it takes to calculate
   print(paste("Iteration", i, "takes", Sys.time() - start2, "minutes to complete creating and adding", name_change, "data to dataframe", sep = " "))
@@ -197,7 +232,7 @@ table <- table %>%
                                       "rreef_prob" = "Rocky reef (probable)",
                                       "deep_coralsponge" = "Deep sea coral-sponge",
                                       "continental_shelf" = "Contiental shelf (10km)",
-                                      "methane_bubble" = "Methan bubble streams",
+                                      "methane_bubble" = "Methane bubble streams",
                                       "habitat" = "Habitat (combined)",
                                       "marine_bird" = "Marine seabird",
                                       "fisheries" = "Fisheries",
@@ -209,23 +244,32 @@ table <- table %>%
 # Tornado figures
 ## Nominal change figure
 n <- ggplot(table, aes(x = reorder(dataset_list, n_range), y = n_range)) +
-  #geom_bar(stat = "identity", fill = "lightblue") +
   geom_errorbar(aes(ymin = n_min,
                     ymax = n_max), width = 0.5, color = "darkblue") +
   coord_flip() +
-  labs(x = "Dataset", y = "Change (Nominal)") +
-  theme_minimal()
+  labs(x = "Dataset",
+       y = "Change (Nominal)",
+       title = paste("Nominal change for score fluctuations in", str_to_title(region), sep = " "), 
+       subtitle = "Range of minimum and maximum changes across all hex grid cells in study area",
+       caption = "Calculation: New score - Old score") +
+  theme_bw() +
+  plot_theme
 
 print(n)
 
 ## Percent change figure
 p <- ggplot(table, aes(x = reorder(dataset_list, p_range), y = p_range)) +
-  #geom_bar(stat = "identity", fill = "lightblue") +
   geom_errorbar(aes(ymin = p_min,
                     ymax = p_max), width = 0.5, color = "darkblue") +
   coord_flip() +
-  labs(x = "Dataset", y = "Change (Percent)") +
-  theme_minimal()
+  labs(x = "Dataset",
+       y = "Change (Percent)",
+       title = paste("Percent change for score fluctuations in", str_to_title(region), sep = " "), 
+       subtitle = "Range of minimum and maximum changes across all hex grid cells in study area",
+       caption = "Calculation: ((New score - Old score) / Old Score) * 100") +
+  scale_y_continuous(limits = c(-75, 500), breaks = seq(-100, 500, 50)) +
+  theme_bw() +
+  plot_theme
 
 print(p)
 
@@ -245,7 +289,6 @@ ggsave(n, filename=file.path(figure_dir, paste(region, "nominal_change_tornado_p
 ggsave(p, filename=file.path(figure_dir, paste(region, "percent_change_tornado_plot.tiff", sep = "_")), width=6.5,
        height=4.5, units="in", dpi=600, compression = "lzw")
 
-#####################################
 #####################################
 #####################################
 
