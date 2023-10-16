76b9109b9566badcb47c7b7ccc9c11b6b8ae78ed
bpfree <brian.patrick.free@gmail.com>
2023-10-16 17:01:47 -0400

    Change directory pathname for natural resources submodel geopackage name

--- code/8_nmfs_protected_species.R
+++ code/8_nmfs_protected_species.R
@@ -52,7 +52,7 @@ wind_area_gpkg <- "data/b_intermediate_data/oregon_wind_area.gpkg"
 
 ### Output directories
 #### Analysis directories
-natural_resources_submodel <- "data/c_submodel_data/natural_resources_submodel.gpkg"
+natural_resources_submodel <- "data/c_submodel_data/oregon_natural_resources_submodel.gpkg"
 
 #####################################
 #####################################
